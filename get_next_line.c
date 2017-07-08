6dbd6b4eb556fb41edd7276bd6e4f8f1700270eb
autopylot <wlin@e1z2r5p2.42.us.org>
2017-07-07 19:22:27 -0700

    Fix leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,30 @@
 /*   By: wlin <wlin@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/05 10:43:20 by wlin              #+#    #+#             */
-/*   Updated: 2017/07/07 12:06:40 by wlin             ###   ########.fr       */
+/*   Updated: 2017/07/07 19:21:47 by wlin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
+
+int ft_check_nl(char **line, char **save)
+{
+	char	*pos;
+
+	if((pos = ft_strchr(*line, '\n')))
+	{
+		if (*(*save))
+			free(*save);
+		*save = ft_strdup(pos + 1);
+		*pos = '\0';
+		if (*(*save) == "")
+			free(*save);
+		return (1);
+	}
+	return (0);
+}
+
 
 int	get_next_line(const int fd, char **line)
 {
@@ -19,52 +37,39 @@ int	get_next_line(const int fd, char **line)
 	char			*sptr;
 	char			buffer[BUFF_SIZE + 1];
 	int				ret;
-	char 			*pos;
 
 	if (fd < 0 || !line || (read(fd, buffer, 0) < 0) || BUFF_SIZE < 1)
 		return (-1);
 	*line = ft_strdup(save);
-	if ((pos = ft_strchr(*line, '\n')))
-	{
-		sptr = save;
-		save = ft_strdup(pos + 1);
-		*pos = '\0';
-		free(sptr);
+	if (ft_check_nl(line, &save))
 		return (1);
-	}
 	while ((ret = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
 		buffer[ret] = '\0';
 		sptr = *line;
 		*line = ft_strjoin(*line, buffer);
 		free(sptr);
-		if ((pos = ft_strchr(*line, '\n')))
-		{
-			//sptr = save;
-			save = ft_strdup(pos + 1);
-			//free(sptr);
-			*pos = '\0';
+		if (ft_check_nl(line, &save))
 			return (1);
-		}
 	}
-	return (ret == 0 ? 0 : -1);
+	if (ret == 0 && ft_strlen(*line))
+		return (1);
+
 }
 
-int main(int argc, char **argv)
-{
-	char *line;
-	int fd;
-	int retval;
-
-	if (argc == 2)
-	{
-		puts(" ");
-	}
-	fd = open(argv[1], O_RDONLY);
-	while ((retval = get_next_line(fd, &line)))
-	{
-		printf("%d ", retval);
-		puts(line);
-	}
-	return (0);
-}
+// int main(int argc, char **argv)
+// {
+// 	char *line;
+// 	int fd;
+// 	int retval;
+//
+// 	if (argc != 2)
+// 		return (1);
+// 	fd = open(argv[1], O_RDONLY);
+// 	while ((retval = get_next_line(fd, &line)))
+// 	{
+// 		printf("%d %s\n", retval, line);
+// 		free(line);
+// 	}
+// 	return (0);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,20 @@
 /*   By: wlin <wlin@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/06 19:33:18 by wlin              #+#    #+#             */
-/*   Updated: 2017/07/07 12:04:04 by wlin             ###   ########.fr       */
+/*   Updated: 2017/07/07 18:41:44 by wlin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft.h"
+# include "./libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
 
-#define BUFF_SIZE 9999
+#define BUFF_SIZE 2
 
 int			get_next_line(int const fd, char **line);
+int 		ft_check_nl(char **line, char **save);
 
 #endif
